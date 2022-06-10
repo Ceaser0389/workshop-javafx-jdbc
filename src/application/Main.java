aa451549ab1df9fe3638f15355bf9b724925654c
César Alves <ceaser.alves03@gmail.com>
2022-06-10 20:20:36 -0300

    About view

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,7 +9,11 @@ import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
+    // atributo para ref Scene
+	private static Scene mainScene;
+	
+	
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
@@ -17,7 +21,7 @@ public class Main extends Application {
 			.getResource("/gui/MainView.fxml"));
 			
 			ScrollPane scrollPane  = loader.load();
-			Scene mainScene = new Scene(scrollPane);
+		    mainScene = new Scene(scrollPane);
 			
 			scrollPane.setFitToHeight(true);
 			scrollPane.setFitToWidth(true);
@@ -29,6 +33,11 @@ public class Main extends Application {
 			e.printStackTrace();
 		}
 	}
+	//metodo aux p pegar o atributo mainScene
+	public static Scene getMainScene() {
+		return mainScene;
+	}
+	
 
 	public static void main(String[] args) {
 		launch(args);
